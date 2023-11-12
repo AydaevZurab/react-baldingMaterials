067f15f84fa7859954dbd329a159ec73de7f518e
grozny <h6umid@gmail.com>
2023-11-13 01:48:00 +0300

    fetch request for mock Api. The functionality of adding cards to the cart

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 /* eslint-disable jsx-a11y/alt-text */
 
+import React from "react"
 import Card from "./components/Card";
 import Cart from "./components/Cart";
 import Header from "./components/Header";
 
-const arr = [{
-  name: "Угловая шлифмашина",
-  price: 10999,
-  imageUrl: "img/materials/1.jpeg",
-},
-{
-  name: "Перфаратор",
-  price: 19600,
-  imageUrl: "img/materials/2.jpg",
-},
-{
-  name: "Гаичный Ключ",
-  price: 560,
-  imageUrl: "img/materials/3.jpg",
-},
-{
-  name: "Плоскогубцы",
-  price: 1200,
-  imageUrl: "img/materials/4.jpg",
-},
-];
 
 function App() {
+  const [items, setItems] = React.useState([])
+  const [cartItems, setCartItems] = React.useState([])
+  const [openedCart, setCartOpened] = React.useState(false)
+
+  React.useEffect(() => {
+    fetch('https://654fede5358230d8f0cdcd5b.mockapi.io/items')
+    .then((res) => {
+      return res.json()
+    })
+    .then((json) => {
+      setItems(json)
+    });
+  }, [])
+
+  const onAddToCart = (obj) => {
+    setCartItems(prew => [...prew, obj])
+  }
+
   return (
     <div className="wrapper">
-      <Cart />
-      <Header />
+      {openedCart ? <Cart items={cartItems} onClickClose = {() => setCartOpened(false)} /> : null}
+      <Header onClickCart = {() => setCartOpened(true)} />
       <div className="content">
         <div className="content-search">
           <h1 className="contentH1">Все материалы</h1>
@@ -41,12 +39,12 @@ function App() {
         </div>
         <div className="materials">
 
-          {arr.map((obj) => (
+          {items.map((item) => (
             <Card
-              name={obj.name}
-              price={obj.price}
-              imageUrl={obj.imageUrl}
-              onClick={() => console.log(obj)}
+              name={item.name}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onPlus={(obj) => onAddToCart(obj)}
             />
           ))}
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,33 @@
-function Card(props) {
-  // const onClickButton = () => {
-  //   alert(props.name)
-  // }
+import React from "react";
+
+function Card({imageUrl, name, price, onPlus}) {
+  const [isAdded, setIsAdded] = React.useState(false);
+  const onClickPlus = () => {
+    onPlus({imageUrl, name, price}) 
+    setIsAdded(!isAdded)
+  }
+
+  const [isLiked, unLiked] = React.useState(false)
+  
+  const onClickFavorite = () => {
+    unLiked(!isLiked)
+  }
+
     return (
         <div className="card">
-            <div className="favorite">
-              <img src="img/liked.png" alt="liked" />
+            <div className="favorite" onClick={onClickFavorite}>
+              <img width={17 } height={17} src={isLiked ? "img/liked.png" : "img/unliked.png"} alt="liked" />
             </div>
             <div className="cardMarginLeft">
-              <img className="imageCenter" width={133} height={112} src={props.imageUrl} alt="Materials" />
-              <h5>{props.name}</h5>
+              <img className="imageCenter" width={133} height={112} src={imageUrl} alt="Materials" />
+              <h5>{name}</h5>
               <div className="cardBottom">
                 <div className="space">
                   <div className="price">Цена:</div>
-                  <b>{props.price} руб.</b>
+                  <b>{price} руб.</b>
                 </div>
-                <div className="button" onClick={props.onClick}>
-                  <img src="img/unplus.png" alt="Plus" />
+                <div className="button" onClick={onClickPlus}>
+                  <img src={isAdded ? "img/added.png" : "img/unplus.png"} alt="Plus" />
                 </div>
               </div>
             </div>
